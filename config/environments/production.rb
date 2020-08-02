fd84f163f4111b1a276083bbf841f0b6ad4e838a
Catalina <caliste@fen.uchile.cl>
2020-08-02 14:10:10 -0400

    final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.digest = true
+  config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -93,5 +94,3 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-config.assets.digest = true
-config.serve_static_assets = true
