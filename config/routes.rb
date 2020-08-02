d6a397af11770afada7ad81504dca6df7505ad72
Catalina <caliste@fen.uchile.cl>
2020-08-02 12:35:39 -0400

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  "users/login"
   get  "pages/about_us"
+  get  '/pages/test', to: 'pages#test'
+  get  "pages/index"
   root "pages#index"
 end
